e898aa3ba17e1d3c74eae1f580971c3eb97063c7
Gregory Wehmeier <wehmenstein@gmail.com>
2018-01-27 23:39:36 -0600

    add publications data query

--- pages/index.js
+++ pages/index.js
@@ -5,16 +5,22 @@ import Layout from 'templates/Layout'
 import Articles from 'components/Articles'
 import SideBar from 'components/SideBar'
 
-const query = `*[_type == "article"] {
+const articlesQuery = `*[_type == 'article'] {
   _id,
   title
 }[0...50]
 `
 
+const publicationsQuery = `*[_type == 'publication'] {
+  _id,
+  name
+}`
+
 export default class IndexPage extends React.Component {
   static async getInitialProps() {
     return {
-      articles: await sanity.fetch(query)
+      articles: await sanity.fetch(articlesQuery),
+      publications: await sanity.fetch(publicationsQuery)
     }
   }
 
@@ -24,7 +30,7 @@ export default class IndexPage extends React.Component {
         <div className='grid-container'>
           <div className='grid-x'>
             <div className='cell small-4 grid-x grid-padding-x'>
-              <SideBar />
+              <SideBar publications={this.props.publications}/>
             </div>
             <div className='cell small-8 grid-x grid-padding-x'>
               <Articles articles={this.props.articles}/>

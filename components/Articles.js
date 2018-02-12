28d5f68e1913bf9a6673aee51bcb2ec1f17e78ae
Gregory Wehmeier <wehmenstein@gmail.com>
2018-02-12 13:13:28 -0600

    articles: add basic fade in animation

--- components/Articles.js
+++ components/Articles.js
@@ -1,34 +1,48 @@
 import React from 'react'
-import Link from 'next/link'
+import _get from 'lodash.get'
 
 import Layout from 'layouts/Main'
 import Article from 'components/Article'
 import styles from './styles/articles'
 
+const delay = 100
+
+const defaultStyle = (index) => ({
+  transition: `opacity 300ms ease-in ${delay * (index + 1)}ms`
+})
+
+const transitionStyles = {
+  'enter': {opacity: 0},
+  'entering': {opacity: 1}
+}
+
 export default class Articles extends React.Component {
   state = {
-    items: this.props.articles
+    transitionIn: 'enter'
+  }
+
+  componentDidMount() {
+    this.setState({
+      transitionIn: 'entering'
+    })
   }
 
   render() {
-    let {articles} = this.props
-
+    let articles = _get(this.props, 'articles', [])
+    articles = articles.map((article, i) => (
+      <div className='list__item cell' key={article._id} style={{...transitionStyles[this.state.transitionIn], ...defaultStyle(i)}}>
+        <Article {...article} />
+      </div>
+    ))
+ 
     return (
       <div className='articles-container cell'>
-        <div className='list grid-x grid-padding-x small-up-1 large-up-2 small-padding-collapse'>
-          {articles.length &&
-            articles.map((article, i) => {
-              let transitionValue = `opacity 500ms ease-in-out ${i * 500}ms`
-
-              return (
-                <div className='list__item cell' key={article._id}>
-                  <Article {...article} />
-                </div>
-              )
-            })
-          }
+        <div className='list'>
+          <div className='grid-x grid-padding-x small-up-1 large-up-2 small-padding-collapse'>
+            {articles}
+          </div>
         </div>
-        <style jsx>{styles}</style>
+        <style jsx global>{styles}</style>
       </div>
     )
   }
